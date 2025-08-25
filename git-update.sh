3bd8233ba31b99e63863937c0b9b82188e88534b
lvhu-coder <elvinhu1016@gmail.com>
2025-08-24 19:11:56 -0700

    Fixed git-update.sh

--- git-update.sh
+++ git-update.sh
@@ -6,11 +6,11 @@ git add -A
 git commit -m "$msg"
 git pull
 
-branch = $(git branch --show-current)
+branch=$(git branch --show-current)
 upstream=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null)
 
 
-if [-z "$upstream" ]; then
+if [ -z "$upstream" ]; then
     git push -u origin "$branch"
 else
     git push
